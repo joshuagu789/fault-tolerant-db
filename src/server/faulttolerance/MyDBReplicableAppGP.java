622d43a2e505549cd6c088f1150063f16ba31d08
joshuagu789 <joshuagu789@gmail.com>
2025-12-02 16:57:08 -0500

    setting up for gigapaxos

--- src/server/faulttolerance/MyDBReplicableAppGP.java
+++ src/server/faulttolerance/MyDBReplicableAppGP.java
@@ -14,7 +14,8 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.HashSet;
 import java.util.Set;
-// import java.util.logging.Level;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This class should implement your {@link Replicable} database app if you wish
@@ -44,6 +45,8 @@ public class MyDBReplicableAppGP implements Replicable {
     protected Cluster cluster;
     protected Session session;
 
+	protected static final Logger log = Logger.getLogger(MyDBReplicableAppGP.class.getName());
+
 	/**
 	 * Set this value to as small a value with which you can get tests to still
 	 * pass. The lower it is, the faster your implementation is. Grader* will
@@ -69,23 +72,64 @@ public class MyDBReplicableAppGP implements Replicable {
         cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
         session = cluster.connect(args[0]);
 		// throw new RuntimeException("Not yet implemented");
-		System.out.println("Replicable Giga Paxos started with keyspace " + args[0]);
+		// System.out.println("Replicable Giga Paxos started with keyspace " + args[0]);
+
+		for(int i = 0; i < args.length; i++) {
+			log.log(Level.INFO, "{0}", new Object[]{args[i]});
+		}
+
+		log.log(Level.INFO, "Replicable Giga Paxos started with keyspace {0}", new Object[]{args[0]});
 	}
 
 	/**
-	 * Refer documentation of {@link Replicable#execute(Request, boolean)} to
-	 * understand what the boolean flag means.
-	 * <p>
-	 * You can assume that all requests will be of type {@link
-	 * edu.umass.cs.gigapaxos.paxospackets.RequestPacket}.
-	 *
+	 * This method must handle the request atomically and return true if
+	 * successful or throw an exception or return false. It is the application's
+	 * responsibility to ensure atomicity, i.e., it should return true iff the
+	 * request was successfully executed; if it returns false or throws an
+	 * exception, the application should ensure that the state of the
+	 * application rolls back any partial execution of this request. If it
+	 * returns false or throws an exception, the replica coordination protocol
+	 * may try to re-attempt executing the request.
+	 * 
 	 * @param request
-	 * @param b
-	 * @return
+	 * @param doNotReplyToClient
+	 *            If true, the application is expected to not send a response
+	 *            back to the originating client (say, because this request is
+	 *            part of a post-crash roll-forward or only the "entry replica"
+	 *            that received the request from the client is expected to
+	 *            respond back. If false, the application is expected to either
+	 *            send a response (if any) back to the client via the
+	 *            {@link ClientMessenger} interface or delegate response
+	 *            messaging to paxos via the {@link ClientRequest#getResponse()}
+	 *            interface.
+	 * 
+	 * @return Must return true if and only if the application handled the
+	 *         request successfully. For safety, executing a request must have a
+	 *         deterministic effect on the safety-critical state. If the request
+	 *         is bad and is to be discarded, the application must still return
+	 *         true (after "successfully" discarding it).
+	 * 
+	 *         If the application returns false, the replica coordination
+	 *         protocol (e.g., paxos) might try to repeatedly re-execute it
+	 *         until successful or kill this replica group (
+	 *         {@code request.getServiceName()}) altogether after a limited
+	 *         number of retries, so the replica group may get stuck unless it
+	 *         returns true after a limited number of retries. Thus, with paxos
+	 *         as the replica coordination protocol, returning false is not
+	 *         really an option as paxos has no way to "roll back" a request
+	 *         whose global order has already been agreed upon.
+	 *         <p>
+	 *         With replica coordination protocols other than paxos, the boolean
+	 *         return value could be used in protocol-specific ways, e.g., a
+	 *         primary-backup protocol might normally execute a request at just
+	 *         one replica but relay the request to one or more backups if the
+	 *         return value of {@link #execute(Request)} is false.
 	 */
 	@Override
-	public boolean execute(Request request, boolean b) {
+	public boolean execute(Request request, boolean doNotReplyToClient) {
 		// TODO: submit request to data store
+		log.log(Level.INFO, "Replicable Giga Paxos called execute with request={0} and doNotReplyToClient={1}", new Object[]{request.toString(), doNotReplyToClient});
+
 		throw new RuntimeException("Not yet implemented");
 	}
 
@@ -99,33 +143,61 @@ public class MyDBReplicableAppGP implements Replicable {
 	@Override
 	public boolean execute(Request request) {
 		// TODO: execute the request by sending it to the data store
-		throw new RuntimeException("Not yet implemented");
+		log.log(Level.INFO, "Replicable Giga Paxos called execute with request={0}", new Object[]{request.toString()});
+
+		return true;
+		// throw new RuntimeException("Not yet implemented");
 	}
 
 	/**
-	 * Refer documentation of {@link Replicable#checkpoint(String)}.
-	 *
-	 * @param s
-	 * @return
+	 * Checkpoints the current application state and returns it.
+	 * 
+	 * @param name
+	 * @return Returns the checkpoint state. If the application encounters an
+	 *         error while creating the checkpoint, it must retry until
+	 *         successful or throw a RuntimeException. Returning a null value
+	 *         will be interpreted to mean that the application state is indeed
+	 *         null.
+	 *         <p>
+	 *         Note that {@code state} may simply be an app-specific handle,
+	 *         e.g., a file name, representing the state as opposed to the
+	 *         actual state. The application is responsible for interpreting the
+	 *         state returned by {@link #checkpoint(String)} and that supplied
+	 *         in {@link #restore(String, String)} in a consistent manner.
 	 */
 	@Override
-	public String checkpoint(String s) {
+	public String checkpoint(String name) {
 		// TODO:
-		throw new RuntimeException("Not yet implemented");
+		log.log(Level.INFO, "Replicable Giga Paxos called checkpoint with name={0}", new Object[]{name});
+
+		// throw new RuntimeException("Not yet implemented");
+		return "wassup";
 	}
 
+
 	/**
-	 * Refer documentation of {@link Replicable#restore(String, String)}
-	 *
-	 * @param s
-	 * @param s1
-	 * @return
+	 * Resets the current application state for {@code name} to {@code state}.
+	 * <p>
+	 * Note that {@code state} may simply be an app-specific handle, e.g., a
+	 * file name, representing the state as opposed to the actual state. The
+	 * application is responsible for interpreting the state returned by
+	 * {@link #checkpoint(String)} and that supplied in
+	 * {@link #restore(String, String)} in a consistent manner.
+	 * 
+	 * @param name
+	 * @param state
+	 * @return True if the app atomically updated the state successfully. Else,
+	 *         it must throw an exception. If it returns false, the replica
+	 *         coordination protocol may try to repeat the operation until
+	 *         successful causing the application to get stuck.
 	 */
 	@Override
-	public boolean restore(String s, String s1) {
+	public boolean restore(String name, String state) {
 		// TODO:
-		throw new RuntimeException("Not yet implemented");
+		log.log(Level.INFO, "Replicable Giga Paxos called restore with name={0}, state={1} ", new Object[]{name, state});
 
+		// throw new RuntimeException("Not yet implemented");
+		return true;
 	}
 
 
--- test/GraderFaultTolerance.java
+++ test/GraderFaultTolerance.java
@@ -41,8 +41,8 @@ public class GraderFaultTolerance extends GraderCommonSetup {
 /**
  * True if Gigapaxos being used, false if Zookeeper or anything else.
  */
-public static final boolean GIGAPAXOS_MODE =
-		Boolean.getBoolean(System.getProperty("gigapaxos.mode"));//false;
+public static final boolean GIGAPAXOS_MODE = true;
+		// Boolean.getBoolean(System.getProperty("gigapaxos.mode"));//false;
 
 /**
  * Maximum permitted size of any collection that is used to maintain
